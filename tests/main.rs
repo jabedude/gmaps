660fe34da42dcfa8cd4b7f22d355dfcb23901f9c
Josh Abraham <sinisterpatrician@gmail.com>
2018-04-16 05:30:54 +0000

    add marker test

--- tests/main.rs
+++ tests/main.rs
@@ -1,8 +1,20 @@
 extern crate gmaps;
 
+use gmaps::GoogleMaps;
+
 #[test]
 fn test_all() {
-    use gmaps::GoogleMaps;
     let gm = GoogleMaps::new();
     gm.draw(&"/vagrant/test_map.html");
+}
+
+#[test]
+fn test_marker() {
+    let mut gm = GoogleMaps::new();
+    let location = (37.770776, -122.461689);
+    gm.new_marker(location);
+    gm.draw(&"/vagrant/test_map.html");
+    println!("{}", gm);
+    // TODO
+    // assert_eq!(gm.markers[0], (37.770776, -122.461689));
 }
